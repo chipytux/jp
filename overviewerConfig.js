0ba13242afff2e52c0a1f67421c1aabaf835d314
Dino Aguilar <chipytux@gmail.com>
2022-12-15 16:23:53 -0300

    3

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1671057254",
+        "cacheTag": "1671131880",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1671041792,
+            "last_rendertime": 1671129169,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
